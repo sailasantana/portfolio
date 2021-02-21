670c36baf8019b64909007e4f3fafe9e933ee2ad
sailasantana94 <sk4862@nyu.edu>
2021-02-21 14:39:15 -0500

    html validator errors corrected

--- interactivity.js
+++ interactivity.js
@@ -1,21 +1,25 @@
 // navigate through the app with event listener
 function navigationListener() {
     $("#aboutNav").on("click", function(event) {
+      $(".menu").toggleClass(".active")
       $(".about").fadeIn(400).removeClass(".hidden"); 
       $(".container, .projects , .contact").hide().addClass('hidden'); 
     });
 
     $("#projectsNav").on('click', function(event) {
+      $(".menu").toggleClass(".active")
       $(".projects").fadeIn(400).removeClass(".hidden"); 
-      $(".container,.contact,.about ").hide().addClass('hidden');     
+      $(".container,.contact,.about").hide().addClass('hidden');     
     });
   
     $('#contactNav').on('click', function(event){
+      $(".menu").toggleClass(".active")
       $(".contact").fadeIn(400).removeClass('.hidden');
-      $(".container,.projects,.about ").hide().addClass('hidden');     
+      $(".container,.projects,.about").hide().addClass('hidden');     
     });
      
     $('#homeNav').on('click', function(event){
+      $(".menu").toggleClass(".active")
       $(".container").fadeIn(400).removeClass('.hidden');
       $(".contact,.projects,.about").hide().addClass('hidden');
     })
