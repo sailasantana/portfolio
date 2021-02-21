34d9b24c73d261f8b651c5263c6f9338366cbddf
sailasantana94 <sk4862@nyu.edu>
2021-02-21 14:57:56 -0500

    footer added

--- interactivity.js
+++ interactivity.js
@@ -1,25 +1,25 @@
 // navigate through the app with event listener
 function navigationListener() {
     $("#aboutNav").on("click", function(event) {
-      $(".menu").toggleClass(".active")
+      $(".toggler").prop("checked", false);
       $(".about").fadeIn(400).removeClass(".hidden"); 
       $(".container, .projects , .contact").hide().addClass('hidden'); 
     });
 
     $("#projectsNav").on('click', function(event) {
-      $(".menu").toggleClass(".active")
+      $(".toggler").prop("checked", false);
       $(".projects").fadeIn(400).removeClass(".hidden"); 
       $(".container,.contact,.about").hide().addClass('hidden');     
     });
   
     $('#contactNav').on('click', function(event){
-      $(".menu").toggleClass(".active")
+      $(".toggler").prop("checked", false);
       $(".contact").fadeIn(400).removeClass('.hidden');
       $(".container,.projects,.about").hide().addClass('hidden');     
     });
      
     $('#homeNav').on('click', function(event){
-      $(".menu").toggleClass(".active")
+      $(".toggler").prop("checked", false);
       $(".container").fadeIn(400).removeClass('.hidden');
       $(".contact,.projects,.about").hide().addClass('hidden');
     })
